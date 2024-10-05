f7fc4c62d16b81097df82fc55b530d3c54ce9186
yusufergin38 <ysfrgn38@gmail.com>
2024-10-06 01:02:33 +0300

    210924_ders

--- 04_Scope/obj/Debug/net8.0/04_Scope.AssemblyInfo.cs
+++ 04_Scope/obj/Debug/net8.0/04_Scope.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04_Scope")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d4224f74a58f145f9cd69e805391b2846b3d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("04_Scope")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04_Scope")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 05_İF_ELSE/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace _05_İF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Değer Giriniz: ");
+            //int yas = Convert.ToInt32(Console.ReadLine());
+            //if (yas > 0)
+            //{
+            //    Console.Write("Pozitif bir değer girdiniz ");
+
+            //}
+            //else if (yas < 0)
+            //{
+
+            //    Console.Write("Negatif bir değer girdiniz ");
+            //}
+            ////else if (yas == 0)
+            ////{
+
+            ////    Console.WriteLine("0 girdiniz");
+            ////}
+            //else
+            //{
+            //    Console.Write("0 girdiniz");
+            //}
+            /*
+            string belge1 = "Takdir";
+            string belge2 = "Teşekkür";
+            while (true)    
+
+            {
+                
+                Console.WriteLine("Not Giriniz: ");
+                int not = Convert.ToInt32(Console.ReadLine());
+                if (not > 0 && not <= 44)
+                {
+                    Console.WriteLine("Kaldınız ");              
+                }
+                else if (not > 44 && not <= 59)
+                {
+                    Console.WriteLine("Geçtiniz ");                  
+                }
+                else if (not > 59 && not <= 84)
+                {
+                    Console.WriteLine($"{belge2} aldınız ");                   
+                }
+                else if (not > 84 && not <= 100)
+                {
+                    Console.WriteLine($"{belge1} e şayansınız ");                  
+                }
+                else
+                {
+                    Console.Write("Böyle bir not olamaz");          
+                }
+                Thread.Sleep(2000); //uygulamayı 3 saniye durdurdu
+                Console.Clear();// console temizlendi
+            */
+            int maas = 17000;
+            int yeni_maas = 0;
+           
+                Console.WriteLine("yaş Giriniz: ");
+                int yas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Cocuk sayısı giriniz ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (yas >= 0 && yas =< 45)
+            {
+                Console.WriteLine("Çalışmaya devam ");
+            }
+            else if (sayi > 0)
+            {
+                Console.WriteLine("Emekli olabilirsiniz");
+                yeni_maas = maas + (sayi * 200);
+                Console.WriteLine("yeni maaş" + maas);
+
+            }
+            else {
+
+                yeni_maas = maas + 200;
+                Console.WriteLine("eski maaş"+maas);
+                Console.WriteLine("yeni maas"+yeni_maas);
+                Console.WriteLine("cocuk sayisi"+sayi);
+                Console.WriteLine(  );
+            }
+                
+               
+  
+            
+
+
+
+
+
+        
+        }
+    }
+}
+
+
